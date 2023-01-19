3b264bf8d9ca77e31c676a1e69c40a0ea80b4e0f
ia popkhadze <iafofkhadze@gmail.com>
2023-01-19 00:43:35 -0800

    current task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Navigation from "./components/Navigation/Navigation";
+import MostSolidProduct from "./components/MostSolid/MostSolidProduct";
+import { createContext, useState } from "react";
+
+export const useProps = createContext(null);
 
 function App() {
+  const [mostSolidId, setMostSolidId] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <useProps.Provider value={{mostSolidId, setMostSolidId}}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/mostsolidproducts" element={<MostSolidProduct />} />
+        </Routes>
+      </Router>{" "}
+    </useProps.Provider>
   );
 }
 
